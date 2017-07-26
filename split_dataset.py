274ac546ab2e8c885d21ebd54bb6ffbf626f0b33
hankchen1728 <hankchen1728@gmail.com>
2017-07-26 17:58:21 +0800

    Add files via upload

---
+++ split_dataset.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+import random,os
+import shutil
+random.seed(10)
+
+def get_filenames(folder_dir):
+    filenames = os.listdir(folder_dir)
+    not_image = []
+    for filename in filenames:
+        if not '.jpg' in filename:
+            not_image.append(filename)
+    for filename in not_image:
+        filenames.remove(filename)
+    return filenames
+
+def build_subfolder(folder_dir):
+    name_list = ['train','validation']
+    for name in name_list:
+        subfolder_dir = os.path.join(folder_dir,name)
+        if not os.path.exists(subfolder_dir):
+            os.mkdir(subfolder_dir)
+
+if __name__ == '__main__':
+    DIR = input('input folder directory:')
+    build_subfolder(DIR)
+    filenames = get_filenames(DIR)
+    print(filenames)
+
+    num = len(filenames)
+    train_num = num*4//5
+    val_num = num//5
+    random.shuffle(filenames)
+    train = filenames[:train_num]
+    validation = filenames[train_num:val_num+train_num]
+    test = filenames[val_num+train_num:]
+
+    name_list = ['train','validation']
+
+    for i in range(len(three_list)):
+        filenames = three_list[i]
+        print(filenames)
+        folder_name = name_list[i]
+        for filename in filenames:
+            file_dir = os.path.join(DIR,filename)
+            new_dir = os.path.join(DIR,folder_name,filename)
+            shutil.move(file_dir, new_dir)
